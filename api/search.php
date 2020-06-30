9289f240a257c875f288689ce2ec1f1ee6f7bf4a
nox-0202 <fernandinho.nco@gmail.com>
2020-06-30 11:40:13 -0300

    search feature

--- api/search.php
+++ api/search.php
@@ -2,15 +2,14 @@
 
     define('BASE_URL', 'https://newsapi.org/v2');
 
-    define('HEADLINES', '/top-headlines?country=pt&apiKey=d484934f136d4a6d91ced68942e52880');
+    $endpoint = "/everything?q={$_POST["words"]}&apiKey=d484934f136d4a6d91ced68942e52880";
 
-   
+
     $req = curl_init();
     curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($req, CURLOPT_URL, BASE_URL.HEADLINES);
+    curl_setopt($req, CURLOPT_URL, BASE_URL.$endpoint);
     $response = json_decode(curl_exec($req));
     curl_close($req);
 
     echo json_encode($response->articles);
-    
 
--- js/req.js
+++ js/req.js
@@ -1,15 +1,14 @@
 function render(articles) {
-    console.log(articles)
-
+    $('#result').html('')
     articles.forEach(element => {
         if (element.description != null) {
-            $('#result').append(`<article><header><h2>${element.title}</h2><img src="${element.urlToImage}" width="250"/></header><p>${element.description}</p></article>`)     
+            $('#result').append(`<article><header><h2>${element.title}</h2><img src="${element.urlToImage}" width="250"/></header><p>${element.description}</p><a herf="${element.url}" target="_blank"></a></article>`)     
         }
     })
 }
 
 $.ajax({
-    url: './../api/Headlines.php',
+    url: './api/Headlines.php',
     type: 'GET',
     data: {}, 
     success: response => {
@@ -17,3 +16,17 @@ $.ajax({
     }
 })
 
+function search() {
+
+    $.ajax({
+        url: './api/search.php',
+        type: 'POST',
+        data: {
+            words: $('#search').val()
+        }, 
+        success: response => {
+            render(JSON.parse(response))
+        }
+    })
+    
+}
