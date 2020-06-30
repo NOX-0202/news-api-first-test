aa2ec45bac37bcd1d8e147b26c6c6b31d7f81337
nox-0202 <fernandinho.nco@gmail.com>
2020-06-30 12:23:10 -0300

    localstorage stuff

--- api/search.php
+++ api/search.php
@@ -1,8 +1,10 @@
 <?php
 
+    $words = str_replace(' ', '%20',$_POST["words"]);
+
     define('BASE_URL', 'https://newsapi.org/v2');
 
-    $endpoint = "/everything?q={$_POST["words"]}&apiKey=d484934f136d4a6d91ced68942e52880";
+    $endpoint = "/everything?q={$words}&apiKey=d484934f136d4a6d91ced68942e52880";
 
 
     $req = curl_init();
--- js/req.js
+++ js/req.js
@@ -2,7 +2,27 @@ function render(articles) {
     $('#result').html('')
     articles.forEach(element => {
         if (element.description != null) {
-            $('#result').append(`<article><header><h2>${element.title}</h2><img src="${element.urlToImage}" width="250"/></header><p>${element.description}</p><a herf="${element.url}" target="_blank"></a></article>`)     
+            $('#result').append(`<article><header><h2>${element.title}</h2><img src="${element.urlToImage}" width="250"/></header><p>${element.description}</p><a href="${element.url}" target="_blank">Leia mais >>></a></article>`)     
+        }
+    })
+}
+
+function save(search) {
+    
+}
+
+function search() {
+
+    $.ajax({
+        url: './api/search.php',
+        type: 'POST',
+        data: {
+            words: $('#search').val()
+        }, 
+        success: response => {
+            render(JSON.parse(response))
+            save($('#search').val())
+            $('#search').val('')
         }
     })
 }
@@ -15,18 +35,3 @@ $.ajax({
         render(JSON.parse(response))
     }
 })
-
-function search() {
-
-    $.ajax({
-        url: './api/search.php',
-        type: 'POST',
-        data: {
-            words: $('#search').val()
-        }, 
-        success: response => {
-            render(JSON.parse(response))
-        }
-    })
-    
-}
